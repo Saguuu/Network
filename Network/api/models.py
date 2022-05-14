f8c10c39c9a67753e2a16daa0edfb4ce2c9d7e54
Saguuu <hjaros1997@gmail.com>
2022-05-13 22:45:07 -0500

    Initial commit

--- Network/api/models.py
+++ Network/api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class siteUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class Post(models.Model):
+    poster = models.ForeignKey(siteUser, on_delete=models.CASCADE, related_name="user_posts")
+    content = models.CharField(max_length=100, default="")
+    likes = models.IntegerField()
+    date = models.DateTimeField(auto_now=True)
+
+class Follow(models.Model):
+    follower = models.ForeignKey(siteUser, on_delete=models.CASCADE, related_name="user_follows")
+    followee = models.ForeignKey(siteUser, on_delete=models.CASCADE, related_name="user_followed")
+    date = models.DateTimeField(auto_now=True)
--- Network/api/views.py
+++ Network/api/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
+
+@api_view(["GET"])
+def apiOverview(request):
+    api_urls = {
+        "Post": "/post/",
+        "User": "/user/",
+        "Single Post": "/post/<str:pk>/",
+        "Create Post": "/post-create/",
+        "Update Post": "/post-update/<str:pk>/",
+        "Delete Post": "/post-delete/<str:pk>/",
+        "Follow User": "/follow-user/<str:pk>/",
+        "Unfollow User": "/unfollow-user/<str:pk>/"
+    }
+    return Response(api_urls)
