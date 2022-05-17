8efa82809110549e93e5ad67692ff6b8c39e8c21
Saguuu <hjaros1997@gmail.com>
2022-05-17 18:04:29 -0500

    Improve serializing of following and user models to make user by frontend easier

--- Network/api/serilializers.py
+++ Network/api/serilializers.py
@@ -3,21 +3,33 @@ from .models import Post, siteUser, Follow
 from django.contrib.auth.models import User
 
 class UserSerializer(serializers.ModelSerializer):
+    
     class Meta:
         model = User
         fields = "__all__"
 
 class PostSerializer(serializers.ModelSerializer):
+    poster = serializers.CharField(source="poster.user.username")
+    pooster_image = serializers.CharField(source="poster.image")
+    
     class Meta:
         model = Post
         fields = "__all__"
 
 class FollowSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(source="followee.user.username")
+    image = serializers.CharField(source="followee.image")
+    follower = serializers.CharField(source="follower.user.username")
+    followee = serializers.CharField(source="followee.user.username")
+
     class Meta:
         model = Follow
         fields = "__all__"
 
 class siteUserSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(source="user.username")
+    authenticated = serializers.BooleanField(source="user.is_authenticated")
+
     class Meta:
         model = siteUser
         fields = "__all__"
