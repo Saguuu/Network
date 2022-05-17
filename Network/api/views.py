9c3436b6a6197cc354583a2ffe8245483032605a
Saguuu <hjaros1997@gmail.com>
2022-05-16 22:29:00 -0500

    Add follow and user related api views

--- Network/api/views.py
+++ Network/api/views.py
@@ -1,19 +1,107 @@
 from django.shortcuts import render
+from itsdangerous import Serializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serilializers import PostSerializer, FollowSerializer, UserSerializer
+from .models import Post, Follow, siteUser
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 @api_view(["GET"])
 def apiOverview(request):
+
     api_urls = {
-        "Post": "/post/",
-        "User": "/user/",
-        "Single Post": "/post/<str:pk>/",
+        "Post": "/post-list/",
+        "User": "/user-list/",
+        "Follow": "/follow-list/<str:pk>/",
+        "Single Post": "/post-single/<str:pk>/",
         "Create Post": "/post-create/",
         "Update Post": "/post-update/<str:pk>/",
         "Delete Post": "/post-delete/<str:pk>/",
-        "Follow User": "/follow-user/<str:pk>/",
-        "Unfollow User": "/unfollow-user/<str:pk>/"
+        "Follow User": "/follow-user/",
+        "Unfollow User": "/unfollow-user/"
     }
+
     return Response(api_urls)
+
+@api_view(["GET"])
+def userList(request):
+
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+
+    return Response(serializer.data)
+
+@api_view(["GET"])
+def postList(request):
+
+    posts = Post.objects.all()
+    serializer = PostSerializer(posts, many=True)
+
+    return Response(serializer.data)
+
+@api_view(["GET"])
+def postSingle(request, pk):
+
+    post = Post.objects.get(id=pk)
+    serializer = PostSerializer(post, many=False)
+
+    return Response(serializer.data)
+
+@api_view(["POST"])
+def postCreate(request):
+
+    serializer = PostSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+@api_view(["POST"])
+def postUpdate(request, pk):
+    
+    post = Post.objects.get(id=pk)
+    serializer = PostSerializer(instance=post, data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+@api_view(["DELETE"])
+def postDelete(request, pk):
+    
+    post = Post.objects.get(id=pk)
+    post.delete()
+
+    return Response("Post Deleted")
+
+@api_view(["GET"])
+def followList(request, pk):
+
+    user = User.objects.get(id=pk)
+    site_user = siteUser.objects.get(user=user)
+    following = site_user.user_follows.all()
+    serializer = FollowSerializer(following, many=True)
+
+    return Response(serializer.data)
+
+@api_view(["POST"])
+def followFollow(request):
+
+    serializer = FollowSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+@api_view(["POST"])
+def followUnfollow(request):
+    
+    unfollow = Follow.objects.all().filter(follower=request.data.get("follower"), followee=request.data.get("followee"))
+    unfollow.delete()
+
+    return Response("User Unfollowed")
