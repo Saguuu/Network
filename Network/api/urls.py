b593a4629ff6f7fffe717856350a3aaeeb6af50d
Saguuu <hjaros1997@gmail.com>
2022-05-16 22:29:20 -0500

    Add new api routes to url file

--- Network/api/urls.py
+++ Network/api/urls.py
@@ -2,5 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.apiOverview, name="api-overview")
+    path("", views.apiOverview, name="api-overview"),
+    path("user-list/", views.userList, name="user-list"),
+    path("post-list/", views.postList, name="post-list"),
+    path("post-single/<str:pk>/", views.postSingle, name="post-single"),
+    path("post-create/", views.postCreate, name="post-create"),
+    path("post-update/<str:pk>/", views.postUpdate, name="post-update"),
+    path("post-delete/<str:pk>/", views.postDelete, name="post-delete"),
+    path("follow-list/<str:pk>/", views.followList, name="follow-list"),
+    path("follow-follow/", views.followFollow, name="follow-follow"),
+    path("follow-unfollow/", views.followUnfollow, name="follow-unfollow")
 ]
