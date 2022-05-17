b4364c111c3545947528f8057f80d55f59e9c70d
Saguuu <hjaros1997@gmail.com>
2022-05-17 15:21:20 -0500

    Add nav component

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -1,16 +1,46 @@
 import React from 'react';
 import "./Nav.css";
 import HomeIcon from '@mui/icons-material/Home';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import TagIcon from '@mui/icons-material/Tag';
+import PersonIcon from '@mui/icons-material/Person';
+import EmailIcon from '@mui/icons-material/Email';
 
 const Nav = () => {
   return (
     <div className="nav">
-        <div className="nav__item">
-            <HomeIcon fontSize="large" className="nav__itemImage">
-                
-            </HomeIcon>
-            <div className="nav__itemText">
-                <h3>Home</h3>
+        <div className="nav__container">
+            <div className="nav__item">
+                <TwitterIcon fontSize="large" className="nav__itemImage">
+                </TwitterIcon>
+            </div>
+            <div className="nav__item">
+                <HomeIcon fontSize="large" className="nav__itemImage"> 
+                </HomeIcon>
+                <div className="nav__itemText">
+                    <h3>Home</h3>
+                </div>
+            </div>
+            <div className="nav__item">
+                <TagIcon fontSize="large" className="nav__itemImage"> 
+                </TagIcon>
+                <div className="nav__itemText">
+                    <h3>Following</h3>
+                </div>
+            </div>
+            <div className="nav__item">
+                <EmailIcon fontSize="large" className="nav__itemImage"> 
+                </EmailIcon>
+                <div className="nav__itemText">
+                    <h3>Messages</h3>
+                </div>
+            </div>
+            <div className="nav__item">
+                <PersonIcon fontSize="large" className="nav__itemImage"> 
+                </PersonIcon>
+                <div className="nav__itemText">
+                    <h3>Profile</h3>
+                </div>
             </div>
         </div>
     </div>
