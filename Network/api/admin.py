01932dc9cd67ded46a78a4874e9cb7d974cf3f33
Saguuu <hjaros1997@gmail.com>
2022-05-16 22:27:53 -0500

    Add tables to admin panel

--- Network/api/admin.py
+++ Network/api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import siteUser, Post, Follow
 
 # Register your models here.
+admin.site.register(siteUser)
+admin.site.register(Post)
+admin.site.register(Follow)
