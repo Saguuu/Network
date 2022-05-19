9edffc540d2dc8d83163e61a8b32ebbd4d2adfff
Saguuu <hjaros1997@gmail.com>
2022-05-19 02:31:11 -0500

    Add and style Post component

---
+++ frontend/src/components/Post.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import "./Post.css";
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+import FavoriteBorderOutlined from '@mui/icons-material/FavoriteBorderOutlined';
+
+const Post = ({ id, poster, image, content, likes, date }) => {
+    return (
+        <div className="post">
+            <div className="post__header">
+                <div className="post__headerLeft">
+                    <img className="post__headerImage" src={ image } />
+                    <h3 className="post__headerName">{ poster }</h3>
+                </div>
+                <div className="post__headerRight">
+                    <p>{ date }</p>
+                </div>
+            </div>
+            <div className="post__content">
+                <p>{ content }</p>
+            </div>
+            <div className="post__toolbar">
+                <div className="post__toolbarLikes">
+                    <FavoriteBorderOutlinedIcon fontSize="small">
+                    </FavoriteBorderOutlinedIcon>
+                    <p>{ likes }</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Post
