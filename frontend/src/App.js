b0e554c2a7cd9ea33b02919a003a40bc3f2cb39b
Saguuu <hjaros1997@gmail.com>
2022-05-20 22:18:09 -0500

    Wrap auth context around app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,18 @@
 import Home from "./components/Home";
 import Login from "./components/Login";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
 
 function App() {
     return (
         <Router>
             <div className="app">
-              <Routes>
-                <Route path="/login" element={<Login />}></Route>
-                <Route path="/" element={<Home />} exact></Route>
-              </Routes>
+              <AuthProvider>
+                <Routes>
+                  <Route path="/login" element={<Login />}></Route>
+                  <Route path="/" element={<Home />} exact></Route>
+                </Routes>
+              </AuthProvider>
             </div>
       </Router>
     );
