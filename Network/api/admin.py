ee7a62290249671a6946b785620974528ed65f69
Saguuu <hjaros1997@gmail.com>
2022-05-25 21:22:07 -0500

    new models to admin

--- Network/api/admin.py
+++ Network/api/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import siteUser, Post, Follow
+from .models import siteUser, Post, Follow, Like, Comment
 
 # Register your models here.
 admin.site.register(siteUser)
 admin.site.register(Post)
 admin.site.register(Follow)
+admin.site.register(Like)
+admin.site.register(Comment)
--- Network/api/models.py
+++ Network/api/models.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import User
 class siteUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="site_user")
     image = models.URLField(default="https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg")
+    bio = models.TextField(default="")
 
     def __str__(self):
         return f"{self.user}"
@@ -13,11 +14,10 @@ class siteUser(models.Model):
 class Post(models.Model):
     poster = models.ForeignKey(siteUser, on_delete=models.CASCADE, related_name="user_posts")
     content = models.CharField(max_length=100, default="")
-    likes = models.IntegerField(default=0)
     date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"{self.poster.user.username}'s post"
+        return f"{self.poster.user.username}'s post {self.id}"
 
 class Follow(models.Model):
     follower = models.ForeignKey(siteUser, on_delete=models.CASCADE, related_name="user_follows")
@@ -27,4 +27,19 @@ class Follow(models.Model):
     def __str__(self):
         return f"{self.follower} follows {self.followee}"
 
+class Like(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="post_likes")
+    liker = models.ForeignKey(siteUser, on_delete=models.CASCADE, related_name="user_likes")
+    date = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return f"{self.liker} liked {self.post}"
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="post_comments")
+    poster = models.ForeignKey(siteUser, on_delete=models.CASCADE, related_name="user_comments")
+    content = models.CharField(max_length=100, default="")
+    date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.poster} commented on {self.post}"
