716be6c3bae4ec16376be7c3ecc91f7e4b19372c
Saguuu <hjaros1997@gmail.com>
2022-05-25 21:22:44 -0500

    Rework serializers structure, add better siteuser serializer

--- Network/api/serilializers.py
+++ Network/api/serilializers.py
@@ -1,23 +1,8 @@
 from rest_framework import serializers
-from .models import Post, siteUser, Follow
+from .models import Post, siteUser, Follow, Like, Comment
 from django.contrib.auth.models import User
 
-class UserSerializer(serializers.ModelSerializer):
-    
-    class Meta:
-        model = User
-        fields = "__all__"
-
-class PostSerializer(serializers.ModelSerializer):
-    poster = serializers.CharField(source="poster.user.username")
-    poster_image = serializers.CharField(source="poster.image")
-    
-    class Meta:
-        model = Post
-        fields = "__all__"
-
 class FollowSerializer(serializers.ModelSerializer):
-    username = serializers.CharField(source="followee.user.username")
     image = serializers.CharField(source="followee.image")
     follower = serializers.CharField(source="follower.user.username")
     followee = serializers.CharField(source="followee.user.username")
@@ -26,10 +11,42 @@ class FollowSerializer(serializers.ModelSerializer):
         model = Follow
         fields = "__all__"
 
+class LikeSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Like
+        fields = "__all__"
+
+class CommentSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Comment
+        fields = "__all__"
+
+class PostSerializer(serializers.ModelSerializer):
+    poster = serializers.CharField(source="poster.user.username")
+    poster_image = serializers.CharField(source="poster.image")
+    post_likes = LikeSerializer(many=True)
+    post_comments = CommentSerializer(many=True)
+    
+    class Meta:
+        model = Post
+        fields = "__all__"
+
 class siteUserSerializer(serializers.ModelSerializer):
-    username = serializers.CharField(source="user.username")
-    authenticated = serializers.BooleanField(source="user.is_authenticated")
+    user_posts = PostSerializer(many=True)
+    user_follows = FollowSerializer(many=True)
+    user_followed = FollowSerializer(many=True)
+    user_likes = LikeSerializer(many=True)
+    user_comments = CommentSerializer(many=True)
 
     class Meta:
         model = siteUser
+        fields = "__all__"
+
+class UserSerializer(serializers.ModelSerializer):
+    site_user = siteUserSerializer(many=True)
+    
+    class Meta:
+        model = User
         fields = "__all__"
