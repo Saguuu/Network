977379338bc5bf5c011a16ede405e4aea6f7eaff
Saguuu <hjaros1997@gmail.com>
2022-05-28 23:39:35 -0500

    Add profileheader component to profile page

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -5,10 +5,12 @@ import Nav from './Nav';
 import axios from "../axios";
 import AuthContext from '../context/AuthContext';
 import ProfileFeed from './ProfileFeed';
+import ProfileHeader from './ProfileHeader';
 
 const Profile = () => {
 
     const [profileUser, setProfileUser] = useState([]);
+    const [profileLikes, setProfileLikes] = useState([]);
     const { userId } = useParams();
     let {user, authTokens, logoutUser} = useContext(AuthContext);
 
@@ -23,7 +25,19 @@ const Profile = () => {
                 console.log(e.response);
             });
         }
+
+        async function fetchUserLikes() {
+            await axios.get(`/api/user-likes/${userId}/`)
+            .then(res => {
+                setProfileLikes(res.data);
+            })
+            .catch(e => {
+                console.log(e.response);
+            });
+        }
+
         fetchUserData();
+        fetchUserLikes();
 
     }, []);
 
@@ -31,9 +45,22 @@ const Profile = () => {
         <div className="profile">
             <div className="profile__container">
                 <Nav />
-                <ProfileFeed 
-                posts={ profileUser.user_posts?.map(post => (post)).reverse() }
-                /> 
+                <div className="profile__content">
+                    <ProfileHeader 
+                    userId={ profileUser.id}
+                    username={ profileUser.username}
+                    image={ profileUser.image}
+                    bio={ profileUser.bio}
+                    follows={ profileUser.user_follows?.length}
+                    followed={ profileUser.user_followed?.length}
+                    />
+                    <ProfileFeed 
+                    posts={ profileUser.user_posts?.map(post => (post)).reverse() }
+                    />
+                    <ProfileFeed 
+                    posts={ profileLikes?.map(post => (post)).reverse() }
+                    />
+                </div>
             </div>
         </div>
     );
