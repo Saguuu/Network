a02a807d81a2b9164616ed0d167618cf4911e0fa
Saguuu <hjaros1997@gmail.com>
2022-05-30 00:23:18 -0500

    Add onclick functions to rest of profileheader elements

--- frontend/src/components/ProfileHeader.js
+++ frontend/src/components/ProfileHeader.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import "./ProfileHeader.css";
 
-const ProfileHeader = ({ userId, username, image, bio, follows, followed }) => {
+const ProfileHeader = ({ userId, username, image, bio, follows, followed, handleClick }) => {
   return (
     <div className="profileheader">
         <div className="profileheader__top">
             <div className="profileheader__topLeft">
-                <img className="profileheader__topLeftImage" src={ image } />
+                <img className="profileheader__topLeftImage" src={ image } alt="" />
                 <h2 className="profileheader__topLeftName">{ username }</h2>
             </div>
             <div className="profileheader__topRight">
@@ -19,20 +19,20 @@ const ProfileHeader = ({ userId, username, image, bio, follows, followed }) => {
             </p>
         </div>
         <div className="profileheader__bottom">
-            <div className="profileheader__bottomOne">
+            <div className="profileheader__bottomOne" onClick={handleClick}>
                 <p className="profileheader__bottomOneNumber">{follows} 
                 <span className="profileheader__bottomOneText">  Following</span></p>
             </div>
-            <div className="profileheader__bottomTwo">
+            <div className="profileheader__bottomTwo" onClick={handleClick}>
                 <p className="profileheader__bottomTwoNumber">{followed} 
                 <span className="profileheader__bottomTwoText">  Followers</span></p>
             </div>
         </div>
         <div className="profileheader__toolbar">
-            <div className="profileheader__toolbarItem"> 
+            <div className="profileheader__toolbarItem" onClick={handleClick} value="posts"> 
                 <p>Posts</p> 
             </div>
-            <div className="profileheader__toolbarItem">
+            <div className="profileheader__toolbarItem" onClick={handleClick}>
                 <p>Likes</p>  
             </div>
         </div>
