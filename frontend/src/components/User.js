9833780f64cf68164216ca0bb4b4d1a9f01759a9
Saguuu <hjaros1997@gmail.com>
2022-05-30 00:24:29 -0500

    Create user component for profile following/followed feeds

---
+++ frontend/src/components/User.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import "./User.css";
+import { Link, } from 'react-router-dom';
+
+const User = ({ id, image, username, bio }) => {
+    return (
+        <div className="user">
+            <div className="user__top">
+                <Link to={`/user/${id}`} style={{ textDecoration: 'none' }}>
+                <div className="user__topLeft">
+                    <img className="user__topLeftImage" src={image} alt="" />
+                    <p className="user__topLeftName">{username}</p>
+                </div>
+                </Link>
+                <div className="user__topRight">
+                    <button className="user__topRightFollow">Follow</button>
+                </div>
+            </div>
+            <div className="user__middle">
+                <p>{bio}</p>
+            </div>
+        </div>
+    );
+}
+
+export default User
