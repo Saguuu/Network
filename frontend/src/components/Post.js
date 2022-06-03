3b09874949f2db269346a9a87e5d8b6b5b9e4a68
Saguuu <hjaros1997@gmail.com>
2022-06-02 23:21:20 -0500

    Add conditional rendering of like button based on if user has liked post or not, also pass state to linked component to correctly render posts list by default when linked to user profile from a profile feed

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -1,13 +1,70 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { Link, } from 'react-router-dom';
 import "./Post.css";
 import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+import FavoriteOutlinedIcon from '@mui/icons-material/FavoriteOutlined';
+import AuthContext from '../context/AuthContext';
+import axios from "../axios";
 
-const Post = ({ id, poster, image, content, likes, date }) => {
+const Post = ({ id, userId, poster, image, content, likes, date }) => {
+
+    let {user, authTokens, fetchUserData} = useContext(AuthContext);
+    let isLiked = false;
+
+    if (user) {
+        for (let i = 0; i < user.likes.length; i++) {
+            if (user.likes[i]["post"] === id) {
+                isLiked = true;
+            }
+        }
+    }
+
+    let like = async (e) => {
+
+        const headers = {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + String(authTokens.access)
+        }
+
+        await axios.post("/api/like-like/", {
+            "liker": user.id,
+            "post": id
+        }, {headers: headers})
+        .then(res => {
+            console.log(res);
+            fetchUserData();
+        })
+        .catch(e => {
+            console.log(e.response);
+        });
+
+    }
+
+    let unLike = async (e) => {
+
+        const headers = {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + String(authTokens.access)
+        }
+
+        await axios.post("/api/like-unlike/", {
+            "liker": user.id,
+            "post": id
+        }, {headers: headers})
+        .then(res => {
+            console.log(res);
+            fetchUserData();
+        })
+        .catch(e => {
+            console.log(e.response);
+        });
+
+    }
+
     return (
         <div className="post">
             <div className="post__header">
-                <Link to={`/user/${id}`} style={{ textDecoration: 'none' }}>
+                <Link to={`/user/${userId}`} state={{fromPost: true}} style={{ textDecoration: 'none' }}>
                 <div className="post__headerLeft">
                     <img className="post__headerImage" src={ image } alt="userImg" />
                     <h3 className="post__headerName">{ poster }</h3>
@@ -22,8 +79,13 @@ const Post = ({ id, poster, image, content, likes, date }) => {
             </div>
             <div className="post__toolbar">
                 <div className="post__toolbarLikes">
-                    <FavoriteBorderOutlinedIcon fontSize="small">
+                    {!isLiked ? (
+                    <FavoriteBorderOutlinedIcon id={ id } className="post__toolbarLikesHeart" fontSize="small" onClick={ like }>
                     </FavoriteBorderOutlinedIcon>
+                    ) : (
+                        <FavoriteOutlinedIcon id={ id } className="post__toolbarLikesHeartLiked" fontSize="small" onClick={ unLike }>
+                    </FavoriteOutlinedIcon>
+                    )}
                     <p>{ likes }</p>
                 </div>
             </div>
